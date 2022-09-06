3e199755e4caa07e3525367c9d95f457b173c2f1
MariaEduardaBortolini <78702896+MariaEduardaBortolini@users.noreply.github.com>
2022-09-06 15:38:47 -0300

    Update salvar-cadastro.php

--- backend/salvar-cadastro.php
+++ backend/salvar-cadastro.php
@@ -8,7 +8,14 @@
     $cadastro->set_cadastro_email($_POST['cadastro_email']);
     $cadastro->set_cadastro_senha($_POST['cadastro_senha']);
     $cadastro->set_cadastro_admin(0);
-    $cadastro->inserir_cadastro();
+
+    if($cadastro->validar($_POST['cadastro_email'])) {
+        $cadastro->inserir_cadastro();
+        echo '<script>alert("Conta cadastrada com sucesso!");window.location = "http://localhost/confeitaria-duda/frontend/index.html"</script>';
+    }
+    else {
+        echo '<script>alert("O email já está cadastrado!");window.location = "http://localhost/confeitaria-duda/frontend/index.html"</script>';
+    }
     
-    header("location: http://localhost/confeitaria-duda/frontend/cardapio.php");
+    //header("location: http://localhost/confeitaria-duda/frontend/index.html");
 ?>
